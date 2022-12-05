1947a75e31389af971f459048289589de6adb507
Nadia <nadia_a@live.co.uk>
2022-12-05 11:44:23 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    void (async () => {
+      const result = await fetch(
+        import.meta.env.VITE_API_URL +
+          "?token=" +
+          import.meta.env.VITE_API_TOKEN
+      );
+      if (result) {
+        const json = result.json();
+        if (json) setData(json);
+        else setData(null);
+      }
+    })();
+  });
+
+  return (
+    <div className="App">
+      <div>
+        <a target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{data && data.msg}</h1>
+      <div className="card">
+        {data && data.data ? data.data : "Setup API token"}
+      </div>
+    </div>
+  );
+}
+
+export default App;
